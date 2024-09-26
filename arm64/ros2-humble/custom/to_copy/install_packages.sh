80e9dec48ef8483f609133f53b323a023a2d0e5c
bwh1270 <imbwh@cau.ac.kr>
2024-09-26 15:51:03 +0900

    arm64 update: adding bash file

--- arm64/ros2-humble/custom/to_copy/install_packages.sh
+++ arm64/ros2-humble/custom/to_copy/install_packages.sh
@@ -31,23 +31,9 @@ apt-get update && apt-get install -q -y \
 #2#
 echo "Start to move the copied files..." 
 
+mv bash .bashrc
 mv aliases .bash_aliases
 mv vimrc .vimrc
 mv tmux .tmux.conf
 mkdir -p .config/ranger
 mv ranger .config/ranger/rc.conf
-
-
-#3#
-echo "Start to modify some lines in .bashrc"
-
-echo "" >> ~/.bashrc
-echo 'parse_git_branch() {' >> ~/.bashrc && \
-echo '    git branch 2> /dev/null | sed -e "/^[^*]/d" -e "s/* \(.*\)/(\1)/"' >> ~/.bashrc && \
-echo '}' >> ~/.bashrc
-echo 'PS1="\[\e[0;31m\][\u]\[\e[m\] \[\e[0;32m\]\w\[\e[m\] \$(parse_git_branch)\[\e[00m\]\n$ "' >> ~/.bashrc
-
-echo 'export EDITOR=vim;'
-
-echo "" >> ~/.bashrc
-echo 'echo "$(stty speed 921600 < /dev/ttyUSB0)"'
